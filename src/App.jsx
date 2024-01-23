d53d1a75063c33485e816211cb3436c55ea3ad46
Justin Wang <jxw6@outlook.com>
2024-01-22 17:24:41 -0700

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,43 @@
-<div>
-</div>
+import { useState } from 'react'
+import { puppyList } from './data.js'
+import './index.css'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null);
+  const [featuredPup2, setfP] = useState(null);
+  //console.log(puppies)
+
+  function handleClick() {
+    // ???
+  }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  //console.log(featuredPup)
+
+  return (
+      <div className="App">
+
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>
+            <div>{puppy.name}</div>
+          </p>
+        );
+      })}
+
+        {featPupId && (
+        <div className="Puppy">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
+      </div>
+);
+}
+
+export default App
